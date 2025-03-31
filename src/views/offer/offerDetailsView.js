f877d85c0f5cbabb2ee08aa52051875c0906ff12
pankowskitomasz <pankowski.email@gmail.com>
2025-03-31 10:33:56 +0200

    routing, layout: signin, signup, passwordupdate, restorepassword

---
+++ src/views/offer/offerDetailsView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function OfferDetailsView(){
+    return(
+        <Container>
+            <h1>Offer Details View</h1>
+
+        </Container>
+    );
+}
+
+export default OfferDetailsView;
